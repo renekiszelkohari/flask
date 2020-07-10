c107453096225982f6adaa944781d55eb45e6e06
Rene <kirenester@gmail.com>
2020-07-10 11:27:00 +0000

    fixed environ bug - os missing

--- run.py
+++ run.py
@@ -3,11 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+
+@app.route('/')
+def hello():
     return "Hello World"
 
-if __name__ == "__main__":
-    app.run(host=environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            degug=True)
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
