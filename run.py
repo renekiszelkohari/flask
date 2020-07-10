5d9ca6e2c22f5b7646445da820e1f97e7aada318
Rene <kirenester@gmail.com>
2020-07-10 11:52:41 +0000

    added templates folder. Rendered index,  about, contact pages

--- run.py
+++ run.py
@@ -1,15 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World"
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
             debug=True)
